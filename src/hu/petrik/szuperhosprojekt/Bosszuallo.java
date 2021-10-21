7e3fbc6dc3f29fdb927ed1a6b3ea18aa8f68c0f5
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-10-21 11:43:43 +0200

    Bosszuálló létrehozva, konstruktor/getterek/setterek létrehozva

---
+++ src/hu/petrik/szuperhosprojekt/Bosszuallo.java
@@ -0,0 +1,50 @@
+package hu.petrik.szuperhosprojekt;
+
+import java.text.Format;
+
+public abstract  class Bosszuallo implements Szuperhos {
+    private double szuperero;
+    private boolean vanEGyengesege;
+
+    public Bosszuallo(double szuperero, boolean vanEGyengesege) {
+        this.szuperero = szuperero;
+        this.vanEGyengesege = vanEGyengesege;
+    }
+
+    public abstract boolean megmentiAVilagot();
+
+    @Override
+    public double mekkoraAzEreje() {
+        return szuperero;
+    }
+
+    @Override
+    public boolean legyoziE(Bosszuallo hos) {
+        if (szuperero > hos.mekkoraAzEreje() && hos.vanEGyengesege) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    public double getSzuperero() {
+        return szuperero;
+    }
+
+    public boolean getVanEGyengesege() {
+        return vanEGyengesege;
+    }
+
+    public void setSzuperero(double szuperero) {
+        this.szuperero = szuperero;
+    }
+
+    public void setVanEGyengesege(boolean vanEGyengesege) {
+        this.vanEGyengesege = vanEGyengesege;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Erő = %f; Van-e gyengesége = %b", szuperero, vanEGyengesege);
+    }
+}
--- src/hu/petrik/szuperhosprojekt/Main.java
+++ src/hu/petrik/szuperhosprojekt/Main.java
@@ -3,5 +3,17 @@ package hu.petrik.szuperhosprojekt;
 public class Main {
 
     public static void main(String[] args) {
+        Bosszuallo b1 = new Bosszuallo(10, true) {
+            @Override
+            public boolean megmentiAVilagot() {
+                return false;
+            }
+
+            @Override
+            public boolean legyoziE(Bosszuallo hos) {
+                return false;
+            }
+        };
+        System.out.println(b1);
     }
 }
