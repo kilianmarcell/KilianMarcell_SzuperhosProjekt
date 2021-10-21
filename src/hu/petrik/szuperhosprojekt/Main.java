470c50f912b5d1d75c616566bb3d5eb2b87170be
Kilián Marcell <kilian.marcell.andras@gmail.com>
2021-10-21 11:04:07 +0200

    Alap

---
+++ src/hu/petrik/szuperhosprojekt/Main.java
@@ -0,0 +1,7 @@
+package hu.petrik.szuperhosprojekt;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ src/hu/petrik/szuperhosprojekt/Szuperhos.java
@@ -0,0 +1,4 @@
+package hu.petrik.szuperhosprojekt;
+
+public interface Szuperhos {
+}
